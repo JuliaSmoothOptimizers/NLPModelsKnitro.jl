62baa621abece126b001ad2892c60dc007a111b2
Dominique Orban <dominique.orban@gmail.com>
2019-04-18 10:21:24 -0400

    implementation for NLSModels
    closes #2

--- src/NLPModelsKnitro.jl
+++ src/NLPModelsKnitro.jl
@@ -4,6 +4,19 @@ export knitro
 
 using NLPModels, KNITRO, SolverTools
 
+# Knitro does not accept least-squares problems with constraints other than bounds.
+# We must treat those as general NLPs.
+# If an NLSModel has constraints other than bounds, we convert it to a FeasibilityFormNLS.
+# Because FeasibilityFormNLS <: AbstractNLSModel, we need a trait to dispatch on.
+_is_general_nlp(nlp::AbstractNLPModel) = Val{true}()
+_is_general_nlp(nls::AbstractNLSModel) = Val{typeof(nls) == FeasibilityFormNLS}()
+
+"""`output = knitro(nlp)`
+
+Solves the `NLPModel` problem `nlp` using KNITRO.
+"""
+knitro(nlp::AbstractNLPModel, args...; kwargs...) = _knitro(_is_general_nlp(nlp), nlp, args...; kwargs...)
+
 const knitro_statuses = Dict(0 => :first_order,
                              # 1 => :first_order,
                              # 2 => :infeasible,
@@ -21,14 +34,10 @@ const knitro_statuses = Dict(0 => :first_order,
                              #-13 => Invalid number detected
                              -300 => :unbounded)
 
-"""`output = knitro(nlp)`
-
-Solves the `NLPModel` problem `nlp` using KNITRO.
-"""
-function knitro(nlp :: AbstractNLPModel;
-                callback :: Union{Function,Nothing} = nothing,
-                ignore_time :: Bool = false,
-                kwargs...)
+function _knitro(::Val{true}, nlp :: AbstractNLPModel;
+                 callback :: Union{Function,Nothing} = nothing,
+                 ignore_time :: Bool = false,
+                 kwargs...)
   n, m = nlp.meta.nvar, nlp.meta.ncon
 
   kc = KNITRO.KN_new()
@@ -177,5 +186,137 @@ function knitro(nlp :: AbstractNLPModel;
                               )
 
 end
+
+
+function _knitro(::Val{false}, nls :: AbstractNLSModel;
+                callback :: Union{Function,Nothing} = nothing,
+                ignore_time :: Bool = false,
+                kwargs...)
+  n, m, ne = nls.meta.nvar, nls.meta.ncon, nls.nls_meta.nequ
+
+  if m > 0
+    @warn "Knitro only treats bound-constrained least-squares problems; converting to feasibility form"
+    return knitro(FeasibilityFormNLS(nls), callback=callback, ignore_time=ignore_time; kwargs...)
+  end
+
+  kc = KNITRO.KN_new()
+  release = KNITRO.get_release()
+  KNITRO.KN_reset_params_to_defaults(kc)
+  KNITRO.KN_set_obj_goal(kc, KNITRO.KN_OBJGOAL_MINIMIZE)
+
+  # add variables and bound constraints
+  KNITRO.KN_add_vars(kc, n)
+
+  lvarinf = isinf.(nls.meta.lvar)
+  if !all(lvarinf)
+    lvar = nls.meta.lvar
+    if any(lvarinf)
+      lvar = copy(nls.meta.lvar)
+      lvar[lvarinf] .= -KTR_INFINITY
+    end
+    KNITRO.KN_set_var_lobnds(kc, lvar)
+  end
+
+  uvarinf = isinf.(nls.meta.uvar)
+  if !all(uvarinf)
+    uvar = nls.meta.uvar
+    if any(uvarinf)
+      uvar = copy(nls.meta.uvar)
+      uvar[uvarinf] .= KTR_INFINITY
+    end
+    KNITRO.KN_set_var_upbnds(kc, uvar)
+  end
+
+  # set primal and dual initial guess
+  KNITRO.KN_set_var_primal_init_values(kc, nls.meta.x0)
+  KNITRO.KN_set_var_dual_init_values(kc,  nls.meta.y0)
+
+  # add number of residual functions
+  KNITRO.KN_add_rsds(kc, ne)
+
+  jrows, jcols = jac_structure_residual(nls)
+
+  # define callback for residual
+  function callbackEvalR(kc, cb, evalRequest, evalResult, userParams)
+    if evalRequest.evalRequestCode != KNITRO.KN_RC_EVALR
+      @warn "callbackEvalR incorrectly called with eval request code " evalRequest.evalRequestCode
+      return -1
+    end
+    residual!(nls, evalRequest.x, evalResult.rsd)
+    return 0
+  end
+
+  # define callback for residual Jacobian
+  function callbackEvalRJ(kc, cb, evalRequest, evalResult, userParams)
+    if evalRequest.evalRequestCode != KNITRO.KN_RC_EVALRJ
+        @warn "callbackEvalRJ incorrectly called with eval request code " evalRequest.evalRequestCode
+        return -1
+    end
+    jac_coord_residual!(nls, evalRequest.x, jrows, jcols, evalResult.rsdJac)
+    return 0
+  end
+
+  # register callbacks
+  cb = KNITRO.KN_add_lsq_eval_callback(kc, callbackEvalR)
+  KNITRO.KN_set_cb_rsd_jac(kc, cb, nls.nls_meta.nnzj, callbackEvalRJ,
+                           jacIndexRsds=convert(Vector{Int32}, jrows .- 1),  # indices must be 0-based
+                           jacIndexVars=convert(Vector{Int32}, jcols .- 1))
+
+  print_output = true
+
+  # Options
+  for (k,v) in kwargs
+    if ignore_time || k != :outlev || v ≥ KNITRO.KN_OUTLEV_SUMMARY
+      KNITRO.KN_set_param(kc, string(k), v)
+    else
+      if v > 0
+        @warn("`outlev` should be ≥ $(KNITRO.KN_OUTLEV_SUMMARY) to get the elapsed time, if you don't care about the elapsed time, pass `ignore_time=true`")
+      end
+      print_output = false
+      KNITRO.KN_set_param(kc, "outlev", KNITRO.KN_OUTLEV_SUMMARY)
+    end
+  end
+
+  # set user-defined callback called after each iteration
+  callback == nothing || KNITRO.KN_set_newpt_callback(kc, callback)
+
+  tmpfile = tempname()
+  local status
+  open(tmpfile, "w") do io
+    redirect_stdout(io) do
+      nStatus = KNITRO.KN_solve(kc)
+    end
+  end
+  knitro_output = readlines(tmpfile)
+
+  Δt = 0.0
+  for line in knitro_output
+    if occursin("CPU time", line)
+      Δt = Meta.parse(split(split(line, "=")[2], "(")[1])
+      break
+    end
+  end
+  if print_output
+    println(join(knitro_output, "\n"))
+  end
+
+  nStatus, obj_val, x, lambda_ = KNITRO.KN_get_solution(kc)
+  primal_feas = KNITRO.KN_get_abs_feas_error(kc)
+  dual_feas = KNITRO.KN_get_abs_opt_error(kc)
+
+  KNITRO.KN_reset_params_to_defaults(kc)
+  KNITRO.KN_free(kc)
+
+  return GenericExecutionStats(get(knitro_statuses, nStatus, :unknown), nls, solution=x,
+                               objective=obj_val, dual_feas=dual_feas,
+                               primal_feas=primal_feas, elapsed_time=Δt,
+                               solver_specific=Dict(:multipliers_con => lambda_[1:m],
+                                                    :multipliers_L => lambda_[m+1:m+n],  # don't know how to get those separately
+                                                    :multipliers_U => [],
+                                                    :internal_msg => nStatus)
+                              )
+
+end
+
 
 end # module
