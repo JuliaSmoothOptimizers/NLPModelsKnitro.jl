36cd255bd6f226db6dcc2c2c88d409cc4489bb63
Dominique Orban <dominique.orban@gmail.com>
2019-04-18 10:21:24 -0400

    implementation for NLSModels
    closes #2

--- test/runtests.jl
+++ test/runtests.jl
@@ -31,7 +31,39 @@ function test_with_callback()
   @test stats.solver_specific[:internal_msg] == KNITRO.KN_RC_USER_TERMINATION
 end
 
+function test_unconstrained_nls()
+  F_Rosen(x) = [x[1] - 1; 10 * (x[2] - x[1]^2)]
+  nls = ADNLSModel(F_Rosen, [-1.2; 1.0], 2)
+  stats = knitro(nls)
+  @test isapprox(stats.objective, 0, atol=1.0e-6)
+  @test isapprox(stats.solution, ones(2), rtol=1e-6)
+end
+
+function test_larger_unconstrained_nls()
+  n = 100
+  F_larger(x) = [[10 * (x[i+1] - x[i]^2) for i = 1:n-1]; [x[i] - 1 for i = 1:n-1]]
+  nls = ADNLSModel(F_larger, 0.9 * ones(n), 2 * (n-1))  # there are local solutions other than ones(n)
+  stats = knitro(nls)
+  @test isapprox(stats.objective, 0, atol=1.0e-6)
+  @test isapprox(stats.solution, ones(n), rtol=1e-6)
+end
+
+function test_constrained_nls()
+  n = 3
+  F_larger(x) = [[10 * (x[i+1] - x[i]^2) for i = 1:n-1]; [x[i] - 1 for i = 1:n-1]]
+  c_quad(x) = [sum(x.^2) - 5; prod(x) - 2]
+  nls = ADNLSModel(F_larger, [0.5; 1.0; 1.5], 2 * (n-1), c=c_quad, lcon=zeros(2), ucon=zeros(2))
+  stats = knitro(nls, opttol=1e-12)
+  # this constrained NLS problem will have been converted to a FeasibilityFormNLS; extract the solution
+  x = stats.solution[1:n]
+  @test isapprox(x, [1.06473, 1.21503, 1.54598], rtol=1e-5)
+end
+
 test_unconstrained()
 test_constrained()
 test_with_params()
 test_with_callback()
+
+test_unconstrained_nls()
+test_larger_unconstrained_nls()
+test_constrained_nls()
