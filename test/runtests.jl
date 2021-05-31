e65879da576388eb1b5951e04c4487e6058279e3
tmigot <tangi.migot@gmail.com>
2021-05-31 19:10:19 +0200

    remove print in tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,7 +7,7 @@ using ADNLPModels, NLPModels, NLPModelsKnitro
 
 function test_unconstrained()
   nlp = ADNLPModel(x -> (x[1] - 1)^2 + 100 * (x[2] - x[1]^2)^2, [-1.2; 1.0])
-  stats = knitro(nlp)
+  stats = knitro(nlp, outlev = 0)
   @test isapprox(stats.solution, [1.0; 1.0], rtol = 1e-6)
   @test stats.status == :first_order
 end
@@ -15,7 +15,7 @@ end
 function test_qp()
   nlp =
     ADNLPModel(x -> (x[1] - 1)^2 + 4 * (x[2] - 3)^2, zeros(2), x -> [sum(x) - 1.0], [0.0], [0.0])
-  stats = knitro(nlp)
+  stats = knitro(nlp, outlev = 0)
   @test isapprox(stats.solution, [-1.4; 2.4], rtol = 1e-6)
   @test stats.iter == 1
   @test stats.status == :first_order
@@ -23,7 +23,7 @@ end
 
 function test_constrained()
   nlp = ADNLPModel(x -> (x[1] - 1)^2 + 4 * (x[2] - 3)^2, zeros(2), x -> [dot(x, x)], [0.0], [1.0])
-  stats = knitro(nlp)
+  stats = knitro(nlp, outlev = 0)
   @test isapprox(stats.solution, [0.11021046172567574, 0.9939082725775202], rtol = 1e-6)
   @test stats.status == :first_order
 
@@ -31,7 +31,7 @@ function test_constrained()
   x0 = copy(stats.solution)
   y0 = copy(stats.multipliers)
   z0 = copy(stats.multipliers_L)
-  stats = knitro(nlp, x0 = x0, y0 = y0, z0 = z0)
+  stats = knitro(nlp, x0 = x0, y0 = y0, z0 = z0, outlev = 0)
   @test stats.status == :first_order
   @test isapprox(stats.solution, [0.11021046172567574, 0.9939082725775202], rtol = 1e-6)
   @test stats.iter == 2
@@ -39,7 +39,7 @@ end
 
 function test_with_params()
   nlp = ADNLPModel(x -> (x[1] - 1)^2 + 100 * (x[2] - x[1]^2)^2, [-1.2; 1.0])
-  stats = knitro(nlp, opttol = 1e-12, presolve = 0)
+  stats = knitro(nlp, opttol = 1e-12, presolve = 0, outlev = 0)
   @test isapprox(stats.solution, [1.0; 1.0], rtol = 1e-6)
   @test stats.status == :first_order
 end
@@ -52,7 +52,7 @@ function test_with_callback()
     return 0
   end
   nlp = ADNLPModel(x -> (x[1] - 1)^2 + 100 * (x[2] - x[1]^2)^2, [-1.2; 1.0])
-  stats = knitro(nlp, opttol = 1e-12, callback = callback)
+  stats = knitro(nlp, opttol = 1e-12, callback = callback, outlev = 0)
   @test stats.solver_specific[:internal_msg] == KNITRO.KN_RC_USER_TERMINATION
   @test stats.iter == 2
   @test stats.status == :exception
@@ -61,7 +61,7 @@ end
 function test_maximize()
   meta = NLPModelMeta(1, x0 = rand(1), lvar = zeros(1), uvar = ones(1), minimize = false)
   nlp = ADNLPModel(meta, Counters(), ADNLPModels.ForwardDiffAD(), x -> x[1], x -> [])
-  stats = knitro(nlp)
+  stats = knitro(nlp, outlev = 0)
   @test isapprox(stats.solution, ones(1), rtol = 1e-6)
   @test isapprox(stats.objective, 1.0, rtol = 1e-6)
   @test isapprox(stats.multipliers_L, -ones(1), atol = 1e-6)
@@ -71,7 +71,7 @@ end
 function test_unconstrained_nls()
   F_Rosen(x) = [x[1] - 1; 10 * (x[2] - x[1]^2)]
   nls = ADNLSModel(F_Rosen, [-1.2; 1.0], 2)
-  stats = knitro(nls)
+  stats = knitro(nls, outlev = 0)
   @test isapprox(stats.objective, 0, atol = 1.0e-6)
   @test isapprox(stats.solution, ones(2), rtol = 1e-6)
   @test stats.status == :first_order
@@ -81,14 +81,14 @@ function test_larger_unconstrained_nls()
   n = 100
   F_larger(x) = [[10 * (x[i + 1] - x[i]^2) for i = 1:(n - 1)]; [x[i] - 1 for i = 1:(n - 1)]]
   nls = ADNLSModel(F_larger, 0.9 * ones(n), 2 * (n - 1))  # there are local solutions other than ones(n)
-  stats = knitro(nls)
+  stats = knitro(nls, outlev = 0)
   @test isapprox(stats.objective, 0, atol = 1.0e-6)
   @test isapprox(stats.solution, ones(n), rtol = 1e-6)
   @test stats.status == :first_order
 
   # test with a good primalinitial guess
   x0 = copy(stats.solution)
-  stats = knitro(nls, x0 = x0)
+  stats = knitro(nls, x0 = x0, outlev = 0)
   @test isapprox(stats.objective, 0, atol = 1.0e-6)
   @test isapprox(stats.solution, ones(n), rtol = 1e-6)
   @test stats.status == :first_order
@@ -109,7 +109,7 @@ function test_constrained_nls()
     zeros(2),
     zeros(2),
   )
-  stats = knitro(nls, opttol = 1e-12)
+  stats = knitro(nls, opttol = 1e-12, outlev = 0)
   # this constrained NLS problem will have been converted to a FeasibilityFormNLS; extract the solution
   x = stats.solution[1:n]
   @test isapprox(x, [1.0647319483656656, 1.21502560462289, 1.5459814546883264], rtol = 1e-5)
