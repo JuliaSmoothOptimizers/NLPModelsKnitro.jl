90fd6719a8cb8576dc58b053b4e445f368f55ef1
dpo <dpo@users.noreply.github.com>
2021-07-10 01:00:26 +0000

    :robot: Format .jl files

--- test/send_gist_url.jl
+++ test/send_gist_url.jl
@@ -12,43 +12,52 @@ TEST_RESULTS_JSON = "test_results.json"
 myauth = GitHub.authenticate(ENV["GITHUB_AUTH"])
 
 function create_gist(authentication)
-    
-    file_content = ""
-    file = open(TEST_RESULTS_FILE, "r")
-    for line in readlines(file)
-        file_content *= line*'\n'
-    end
-    close(file)
-    
-    file_dict = Dict(TEST_RESULTS_FILE => Dict("content" => file_content))
-    gist = Dict{String,Any}("description" => "Test results",
-                             "public" => true,
-                             "files" => file_dict)
-    
-    posted_gist = GitHub.create_gist(params = gist, auth = authentication)
+  file_content = ""
+  file = open(TEST_RESULTS_FILE, "r")
+  for line in readlines(file)
+    file_content *= line * '\n'
+  end
+  close(file)
 
-    return posted_gist
+  file_dict = Dict(TEST_RESULTS_FILE => Dict("content" => file_content))
+  gist = Dict{String, Any}("description" => "Test results", "public" => true, "files" => file_dict)
+
+  posted_gist = GitHub.create_gist(params = gist, auth = authentication)
+
+  return posted_gist
 end
 
 function post_gist_url_to_pr(comment::String; kwargs...)
-    api = GitHub.DEFAULT_API
-    repo = get_repo(api, ENV["org"], ENV["repo"]; kwargs...)
-    pull_request = get_pull_request(api, ENV["org"], repo, parse(Int, ENV["pullrequest"]); kwargs...)
-    GitHub.create_comment(api, repo, pull_request, comment; kwargs...)
+  api = GitHub.DEFAULT_API
+  repo = get_repo(api, ENV["org"], ENV["repo"]; kwargs...)
+  pull_request = get_pull_request(api, ENV["org"], repo, parse(Int, ENV["pullrequest"]); kwargs...)
+  GitHub.create_comment(api, repo, pull_request, comment; kwargs...)
 end
 
 function get_repo(api::GitHub.GitHubWebAPI, org::String, repo_name::String; kwargs...)
-    my_params = Dict(:visibility => "all")
-    # return GitHub.repo(api, repo; params = my_params, kwargs...)
-    return Repo(GitHub.gh_get_json(api, "/repos/$(org)/$(repo_name)"; params = my_params, kwargs...))
+  my_params = Dict(:visibility => "all")
+  # return GitHub.repo(api, repo; params = my_params, kwargs...)
+  return Repo(GitHub.gh_get_json(api, "/repos/$(org)/$(repo_name)"; params = my_params, kwargs...))
 end
 
-function get_pull_request(api::GitHub.GitHubWebAPI, org::String, repo::Repo, pullrequest_id; kwargs...)
-    my_params = Dict(:sort => "popularity",
-                    :direction => "desc")
-    pull_request = PullRequest(GitHub.gh_get_json(api, "/repos/$(org)/$(repo.name)/pulls/$(pullrequest_id)"; params=my_params, kwargs...))
-   
-    return pull_request
+function get_pull_request(
+  api::GitHub.GitHubWebAPI,
+  org::String,
+  repo::Repo,
+  pullrequest_id;
+  kwargs...,
+)
+  my_params = Dict(:sort => "popularity", :direction => "desc")
+  pull_request = PullRequest(
+    GitHub.gh_get_json(
+      api,
+      "/repos/$(org)/$(repo.name)/pulls/$(pullrequest_id)";
+      params = my_params,
+      kwargs...,
+    ),
+  )
+
+  return pull_request
 end
 
 post_gist_url_to_pr("Here are the test results: $(create_gist(myauth).html_url)"; auth = myauth)
