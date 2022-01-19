35a3b4c9eb20f449ea6416f799fe861a445dc9fc
Monssaf Toukal <tmonssaf@gmail.com>
2022-01-18 22:12:34 -0500

    unit tests executed on SGE

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-julia -E 'using Pkg; Pkg.activate(joinpath("temp_env", "NLPModelsKnitro")); Pkg.develop(PackageSpec(url=joinpath("."))); Pkg.build("NLPModelsKnitro"); Pkg.test("NLPModelsKnitro")' &> test_results.txt
+# Specify the module to test (e.g "HSL")
+julia -E 'using Pkg; module_name = "NLPModelsKnitro"; Pkg.activate("test_env"); Pkg.develop(PackageSpec(url=joinpath("."))); Pkg.test(module_name)' &> test_results.txt
 
 # Create the gist and create comment on PR:
 julia test/send_gist_url.jl
-
-
